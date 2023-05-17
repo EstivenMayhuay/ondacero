9934f7c9771a27e88a8d408e8efeb5e058c0b667
GPR <gpr.soporte@gmail.com>
2023-05-17 11:56:17 -0500

    add styles to programs

--- resources/views/components/programs-block.blade.php
+++ resources/views/components/programs-block.blade.php
@@ -1,9 +1,16 @@
+@php
+    use Carbon\Carbon;
+@endphp
+
 <div class="componentPrograms">
-    <div class="programs">
+    <div class="programs d-flex flex-column align-items-center">
         @foreach ($programs as $program)
             <article class="programs__item">
                 <div class="programs__image">
                     <img src="https://ondacero.com.pe/j/images/programacion/{{$program->urlPhoto}}" alt="{{$program->title}}" width="220" height="auto">
+                </div>
+                <div class="programs__info bg-dark text-center text-white p-3 fw-bold">
+                    {{Carbon::parse($program->hora)->format('h:i A')}} a {{Carbon::parse($program->hora2)->format('h:i A')}}
                 </div>
             </article>
         @endforeach
@@ -14,6 +21,25 @@
 
     <style>
 
+        .programs__item {
+            width: calc(100% - 2rem);
+            margin-block: 1rem;
+        }
+
+        .programs__image img  {
+            display: block;
+            width: 100%;
+            height: auto;
+        }
+
+        @media screen and (min-width: 768px) {
+            .programs__item {
+                width: 460px;
+            }
+        }
+
+        @media screen and (min-width: 1024px) {
+        }
     </style>
 
 </b:push>
--- resources/views/programs.blade.php
+++ resources/views/programs.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="sectionPrograms py-5 px-4">
-        <h3>{{$programs->day}}</h3>
+        <h3 class="text-center">{{$programs->day}}</h3>
         <x-programs-block :programs="$programs->results"/>
     </section>
 @endsection
